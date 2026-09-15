f5d4879ad031dd3b3b10b1eada22272cc050e154
vishakha <singh1234vishakha@gmail.com>
2026-09-15 19:21:33 +0530

    Updated Experiment 2 SHA-256 code

--- experiment2/hash.py
+++ experiment2/hash.py
@@ -1,8 +1,22 @@
 import hashlib
 
-text = "HELLO"
+# Hashing a text
+text = "hello"
 
 hash_value = hashlib.sha256(text.encode()).hexdigest()
 
-print("Original:", text)
-print("Hash:", hash_value)
+print("Original text:", text)
+print("SHA-256 hash:", hash_value)
+
+
+# Hashing a file
+sha256_hash = hashlib.sha256()
+
+filename = "experiment 2/sample.txt"
+
+with open(filename, "rb") as file:
+    while chunk := file.read(4096):
+        sha256_hash.update(chunk)
+
+print("Original file:", filename)
+print("SHA-256 hash:", sha256_hash.hexdigest())
